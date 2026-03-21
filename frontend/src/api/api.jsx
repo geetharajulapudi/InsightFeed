07c519c5b04de23d039ae26fa613d7de152d3f59
geetharajulapudi <geetharajulapudi19@gmail.com>
2026-03-21 22:33:39 +0530

    fix: hardcode render backend URL as fallback

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -4,7 +4,7 @@ import { getUserUUID } from "../utils/user";
 // Create an Axios instance with the base URL of the backend API
 const api = axios.create({
   // baseURL: "http://localhost:8000/",
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "https://insightfeed.onrender.com",
 });
 
 export const userUUID = getUserUUID();
