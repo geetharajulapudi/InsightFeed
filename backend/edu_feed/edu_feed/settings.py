183ff09e059de85119669f2b965025f79e4ced66
geetharajulapudi <geetharajulapudi19@gmail.com>
2026-03-21 15:45:21 +0530

    updated some file for deployment

--- backend/edu_feed/edu_feed/settings.py
+++ backend/edu_feed/edu_feed/settings.py
@@ -111,7 +111,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL") or (
 )
 
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, conn_health_checks=True)
 }
 
 # Password validation
