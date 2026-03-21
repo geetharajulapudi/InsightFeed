e93147b728daede33d9a74b310a63115e4316f61
geetharajulapudi <geetharajulapudi19@gmail.com>
2026-03-21 22:24:28 +0530

    fix: use import.meta.env for VITE_API_URL

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -4,7 +4,7 @@ import { getUserUUID } from "../utils/user";
 // Create an Axios instance with the base URL of the backend API
 const api = axios.create({
   // baseURL: "http://localhost:8000/",
-  baseURL: process.env.VITE_API_URL?.replace(/\/$/, "")
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const userUUID = getUserUUID();
